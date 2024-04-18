f27f56f7c777bd477d6c61db34a633cd22587649
lucas <lucasgalbertinazzi98@gmail.com>
2024-04-18 17:07:40 -0300

    V.1

---
+++ Controllers/TarefaController.cs
@@ -0,0 +1,142 @@
+﻿using AppToDoList.Data;
+using AppToDoList.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppToDoList.Controllers
+{
+    public class TarefaController : Controller
+    {
+        private readonly ToListDbContext _context;
+
+        public TarefaController(ToListDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Tarefa
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Tarefas.ToListAsync());
+        }
+
+        // GET: Tarefa/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tarefa = await _context.Tarefas.FirstOrDefaultAsync(m => m.Id == id);
+            if (tarefa == null)
+            {
+                return NotFound();
+            }
+
+            return View(tarefa);
+        }
+
+        // GET: Tarefa/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Tarefa/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Titulo,Descricao,Concluida,DataCriacao,DataConclusao")] TarefaInfo tarefa)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(tarefa);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tarefa);
+        }
+
+        // GET: Tarefa/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tarefa = await _context.Tarefas.FindAsync(id);
+            if (tarefa == null)
+            {
+                return NotFound();
+            }
+            return View(tarefa);
+        }
+
+        // POST: Tarefa/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Titulo,Descricao,Concluida,DataCriacao,DataConclusao")] TarefaInfo tarefa)
+        {
+            if (id != tarefa.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(tarefa);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TarefaExists(tarefa.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tarefa);
+        }
+
+        // GET: Tarefa/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tarefa = await _context.Tarefas.FirstOrDefaultAsync(m => m.Id == id);
+            if (tarefa == null)
+            {
+                return NotFound();
+            }
+
+            return View(tarefa);
+        }
+
+        // POST: Tarefa/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var tarefa = await _context.Tarefas.FindAsync(id);
+            _context.Tarefas.Remove(tarefa);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TarefaExists(int id)
+        {
+            return _context.Tarefas.Any(e => e.Id == id);
+        }
+    }
+}
